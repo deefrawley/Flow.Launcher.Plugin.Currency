716e936a8675fc9886018d2d72fb218897e91d55
Jeremy Wu <jeremy24wu@gmail.com>
2021-06-16 09:09:14 +1000

    add custom lib folder for packages

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import sys,os
+parent_folder_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(parent_folder_path)
+sys.path.append(os.path.join(parent_folder_path, 'lib'))
+sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
 from plugin import Main
 
